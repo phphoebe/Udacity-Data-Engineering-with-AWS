7412ac35864db38581a93f0d798eb5bc2ac48cf9
Phoebe Chen <phoebehch@gmail.com>
2023-06-05 13:21:41 -0400

    Add Docstrings for Python functions

--- Course 2-Cloud Data Warehouses/Project 2-Data Warehouse/create_tables.py
+++ Course 2-Cloud Data Warehouses/Project 2-Data Warehouse/create_tables.py
@@ -4,22 +4,39 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """
+    Drop each table using the queries in `drop_table_queries` list.
+
+    Parameters:
+        cur: (psycopg2.extensions.cursor): The PostgreSQL cursor object.
+        conn: (psycopg2.extensions.connection): The PostgreSQL connection object.
+
+    Returns:
+        None
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """
+    Create each table using the queries in `create_table_queries` list.
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    Main function to drop and create tables for the Sparkify database.
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(
+        *config['CLUSTER'].values()))
     cur = conn.cursor()
 
     drop_tables(cur, conn)
--- Course 2-Cloud Data Warehouses/Project 2-Data Warehouse/etl.py
+++ Course 2-Cloud Data Warehouses/Project 2-Data Warehouse/etl.py
@@ -4,12 +4,25 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    Load data from S3 into staging tables on Redshift using the queries in `copy_table_queries` list.
+
+    Parameters:
+        cur: (psycopg2.extensions.cursor): The PostgreSQL cursor object.
+        conn: (psycopg2.extensions.connection): The PostgreSQL connection object.
+
+    Returns:
+        None
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    Insert data from staging tables into analytics tables on Redshift using the queries in `insert_table_queries` list.
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
@@ -19,9 +32,10 @@ def main():
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(
+        *config['CLUSTER'].values()))
     cur = conn.cursor()
-    
+
     load_staging_tables(cur, conn)
     insert_tables(cur, conn)
 
